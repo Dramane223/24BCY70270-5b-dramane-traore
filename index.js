829e2fcbc001f8ebaa949a7338868d73b111259b
Dramane223 <abadramanetraore85@gmail.com>
2026-03-05 16:30:16 +0530

    Initial commit: Student Management System MVC

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+import studentRoutes from "./routes/student.routes.js";
+import viewRoutes from "./routes/student.view.routes.js";
+import methodOverride from "method-override";
+
+dotenv.config();
+
+const app = express();
+app.use(methodOverride("_method"));
+
+connectDB();
+
+app.set("view engine","ejs");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/students",studentRoutes);
+app.use("/view",viewRoutes);
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+console.log(`Server running on port ${PORT}`);
+});
